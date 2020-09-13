91427e52424a00e42ff885276ceb0e926256803f
AK <icecicle04@gmail.com>
2020-09-12 23:07:03 -0400

    added initial IF formula to change classes for past/future/present

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 console.log("helloworld");
 
-var currentDay = moment().format("LLLL");
-var currentHour = moment();
-var nineAm = "";
-var tenAM = "";
-var elevenAM = "";
-var twelvePM = "";
-var onePM = "";
-var twoPM = "";
-var threePM = "";
-var fourPM = "";
-var fivePM = "";
+var currentDay = moment().format("LL");
+var currentHour = moment().format("k");
+// console.log(currentHour);
 
-console.log(currentDay);
+// IFs to format colors rows
+if (currentHour > 9) {
+  $("#nineAmText").addClass("past");
+} else if (currentHour == 9) {
+  $("nineAmText").addClass("present");
+} else if (currentHour < 9) {
+  $("nineAmText").addClass("future");
+}
+// id="nineAmText"
+// color present
+
+var nineAmNote = $("#onepm");
+var tenAmNote = "";
+var elevenAmNote = "";
+var twelvePmNote = "";
+var onePmNote = "";
+var twoPmNote = "";
+var threePmNote = "";
+var fourPmNote = "";
+var fivePmNote = "";
+
 $("#currentDay").append(currentDay);
 
 // $(document).ready(function () {
