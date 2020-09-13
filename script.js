de178f5cb79ca852ff9592862e5c7fc432ad5ed8
AK <icecicle04@gmail.com>
2020-09-12 23:24:13 -0400

    added remaining if statements for present/future/past classes

--- script.js
+++ script.js
@@ -12,6 +12,64 @@ if (currentHour > 9) {
 } else if (currentHour < 9) {
   $("nineAmText").addClass("future");
 }
+if (currentHour > 10) {
+  $("#tenAmText").addClass("past");
+  console.log("chode");
+} else if (currentHour == 10) {
+  $("tenAmText").addClass("present");
+} else if (currentHour < 10) {
+  $("tenAmText").addClass("future");
+}
+if (currentHour > 11) {
+  $("#elevenAmText").addClass("past");
+} else if (currentHour == 11) {
+  $("elevenAmText").addClass("present");
+} else if (currentHour < 11) {
+  $("elevenAmText").addClass("future");
+}
+if (currentHour > 12) {
+  $("#twelvePmText").addClass("past");
+} else if (currentHour == 12) {
+  $("twelvePmText").addClass("present");
+} else if (currentHour < 12) {
+  $("twelvePmText").addClass("future");
+}
+if (currentHour > 13) {
+  $("#onePmText").addClass("past");
+} else if (currentHour == 13) {
+  $("onePmText").addClass("present");
+} else if (currentHour < 13) {
+  $("onePmText").addClass("future");
+}
+if (currentHour > 14) {
+  $("#twoPmText").addClass("past");
+  console.log("chode");
+} else if (currentHour == 14) {
+  $("twoPmText").addClass("present");
+} else if (currentHour < 14) {
+  $("twoPmText").addClass("future");
+}
+if (currentHour > 15) {
+  $("#threePmText").addClass("past");
+} else if (currentHour == 15) {
+  $("threePmText").addClass("present");
+} else if (currentHour < 15) {
+  $("threePmText").addClass("future");
+}
+if (currentHour > 16) {
+  $("#fourPmText").addClass("past");
+} else if (currentHour == 16) {
+  $("fourPmText").addClass("present");
+} else if (currentHour < 16) {
+  $("fourPmText").addClass("future");
+}
+if (currentHour > 17) {
+  $("#fivePmText").addClass("past");
+} else if (currentHour == 17) {
+  $("fivePmText").addClass("present");
+} else if (currentHour < 17) {
+  $("fivePmText").addClass("future");
+}
 // id="nineAmText"
 // color present
 
