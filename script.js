cdddac0bb18689ff33f22d46c2fead74e5a3c23a
AK <icecicle04@gmail.com>
2020-09-12 22:19:43 -0400

    linked current date to site DYNCAMICALLY

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 console.log("helloworld");
 
-console.log(moment());
+var currentDay = moment().format("LLLL");
+var currentHour = moment();
+var nineAm = "";
+var tenAM = "";
+var elevenAM = "";
+var twelvePM = "";
+var onePM = "";
+var twoPM = "";
+var threePM = "";
+var fourPM = "";
+var fivePM = "";
 
-$(document).ready(function () {
-  var currentDay = moment();
-  var currentHour = moment();
-  var nineAm = "";
-  var tenAM = "";
-  var elevenAM = "";
-  var twelvePM = "";
-  var onePM = "";
-  var twoPM = "";
-  var threePM = "";
-  var fourPM = "";
-  var fivePM = "";
-});
+console.log(currentDay);
+$("#currentDay").append(currentDay);
+
+// $(document).ready(function () {
+//   var currentHour = moment();
+//   var nineAm = "";
+//   var tenAM = "";
+//   var elevenAM = "";
+//   var twelvePM = "";
+//   var onePM = "";
+//   var twoPM = "";
+//   var threePM = "";
+//   var fourPM = "";
+//   var fivePM = "";
+// });
