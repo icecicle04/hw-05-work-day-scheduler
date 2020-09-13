8cc29271ecc098dfb75d17cb2279dd42901b2ea3
AK <icecicle04@gmail.com>
2020-09-12 22:07:38 -0400

    added remaining rows and variables to JS

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ console.log("helloworld");
 
 console.log(moment());
 
-$("<p>");
+$(document).ready(function () {
+  var currentDay = moment();
+  var currentHour = moment();
+  var nineAm = "";
+  var tenAM = "";
+  var elevenAM = "";
+  var twelvePM = "";
+  var onePM = "";
+  var twoPM = "";
+  var threePM = "";
+  var fourPM = "";
+  var fivePM = "";
+});
